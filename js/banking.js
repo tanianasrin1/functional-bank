273a18306dd6c4e1572742154a91a5cdb48b7efa
tanianasrin1 <tasrineva169@gmail.com>
2022-02-14 00:02:47 +0600

    negative error

--- js/banking.js
+++ js/banking.js
@@ -15,13 +15,29 @@ function updateTotalField( totalFieldId , depositeAmount){
     depositeTotal.innerText = newDepositeTotal;
 }
 
+function updateBalance(depositeAmount, isAdd){
+    
+    const balanceTotal = document.getElementById('balance-total')
+    const balanceTotalText = balanceTotal.innerText
+    const previousabalanceTotal = parseFloat(balanceTotalText)
+   if(isAdd == true){
+    const newbalanceTotal = previousabalanceTotal + depositeAmount;
+    balanceTotal.innerText = newbalanceTotal;
+   }
+
+   else{
+    const newbalanceTotal = previousabalanceTotal - depositeAmount;
+    balanceTotal.innerText = newbalanceTotal;
+   }
+}
+
 document.getElementById('deposite-button').addEventListener('click', function(){
 
     // deposite balance
     // const depositeInput = document.getElementById('deposite-input');
     // const depositeAmountText = depositeInput.value;
     // const depositeAmount = parseFloat(depositeAmountText);
-    const depositeAmount = getInputValue('deposite-input');
+    
 
     // update deposite
 
@@ -31,16 +47,21 @@ document.getElementById('deposite-button').addEventListener('click', function(){
     const newDepositeTotal = previousdepositeAmount + depositeAmount;
     depositeTotal.innerText = newDepositeTotal; */
 
-    updateTotalField( 'deposite-total', depositeAmount);
+   
 
     // total balance 
-
+/* 
     const balanceTotal = document.getElementById('balance-total')
     const balanceTotalText = balanceTotal.innerText
     const previousabalanceTotal = parseFloat(balanceTotalText)
     const newbalanceTotal = previousabalanceTotal + depositeAmount;
-    balanceTotal.innerText = newbalanceTotal;
-    
+    balanceTotal.innerText = newbalanceTotal; */
+
+    const depositeAmount = getInputValue('deposite-input');
+    if(depositeAmount > 0){
+        updateTotalField( 'deposite-total', depositeAmount);
+        updateBalance(depositeAmount , true)  
+    }
 })
 
 
@@ -51,7 +72,7 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
     // const withdrawInput = document.getElementById('withdraw-input');
     // const withdrawAmonutText = withdrawInput.value;
     // const withdrawAmount= parseFloat(withdrawAmonutText);
-    const withdrawAmount = getInputValue('withdraw-input');
+
 
     // update withdraw
 
@@ -61,14 +82,18 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
     const newWithdrawTotal = previouswithdrawAmount + withdrawAmount;
     withdrawTotal.innerText = newWithdrawTotal; */
 
-    updateTotalField('withdraw-total' , withdrawAmount);
 
     // withdraw update balance
-    const balanceTotal = document.getElementById('balance-total') 
-    const balanceTotalText = balanceTotal.innerText
-    const previousabalanceTotal = parseFloat(balanceTotalText)
-    const newbalanceTotal = previousabalanceTotal - withdrawAmount ;
-    balanceTotal.innerText = newbalanceTotal;
+    // const balanceTotal = document.getElementById('balance-total') 
+    // const balanceTotalText = balanceTotal.innerText
+    // const previousabalanceTotal = parseFloat(balanceTotalText)
+    // const newbalanceTotal = previousabalanceTotal - withdrawAmount ;
+    // balanceTotal.innerText = newbalanceTotal;
 
+    const withdrawAmount = getInputValue('withdraw-input'); 
+    if(withdrawAmount > 0)  {
+        updateTotalField('withdraw-total' , withdrawAmount);
+        updateBalance(withdrawAmount, false) 
+    } 
    
 })
